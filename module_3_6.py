499d653eff77737f15b40fdb0c0e67de88184350
vvlap <vvlap1976@gmail.com>
2024-12-18 16:04:53 +0300

    Add files via upload

--- module_3_6.py
+++ module_3_6.py
@@ -9,14 +9,16 @@ def calc_sum_struct(data_structure):
     for i in list_struct:                       #считаем сумму
         if "'" in i:
             summa += len(i.replace("'",""))     #если элемент строковый
+        elif "." in i:
+            summa += float(i)
         else:
             summa += int(i)
     return summa
 
 
 result1 = calc_sum_struct([[1, 2, 3],{'a': 4, 'b': 5},(6, {'cube': 7, 'drum': 8}),"Hello",((), [{(2, 'Urban', ('Urban2', 35))}])])
-result2 = calc_sum_struct([1, 2, 'Anna', {'a': 4, 'b': 5}])
-result3 = calc_sum_struct([5,2,"Anna5", [{'ab':4, 'cd':5}, 'Vika']])    #проверка "корявого" ввода структуры данных
+result2 = calc_sum_struct([1, 2.0, 'Anna', {'a': 4, 'b': 5}])
+result3 = calc_sum_struct([5,2,"Anna5", [{'ab':4.0, 'cd':5}, 'Vika']])    #проверка "корявого" ввода структуры данных
 print(result1)
 print(result2)
 print(result3)
